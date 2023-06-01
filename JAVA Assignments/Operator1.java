72d7545eb0e984f1be44bd155b4c27a97ba260e3
AnushreeGajbhiiye <gajbhiyeanushree99@gmail.com>
2023-06-01 16:22:33 +0530

    Arithmatic Operator in Java

---
+++ JAVA Assignments/Operator1.java
@@ -0,0 +1,10 @@
+public class Operator1{
+public static void main(String[] args){
+float a=10,b=20;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+}  
+}
