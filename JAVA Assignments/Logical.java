7bdef2f36730064830d8fc81a8db69754cd8f14e
AnushreeGajbhiiye <gajbhiyeanushree99@gmail.com>
2023-06-01 17:44:25 +0530

    Java Logical Operator

---
+++ JAVA Assignments/Logical.java
@@ -0,0 +1,10 @@
+class Logical{
+public static void main(String[] args){
+	
+ System.out.println(10<5 && 2>5);
+ System.out.println(10>5 || 3>5);
+ 
+ boolean condition = false;
+ System.out.println(!condition);
+}
+}
