373e8290f907806246c95a63d9623a6f32cb0950
AnushreeGajbhiiye <gajbhiyeanushree99@gmail.com>
2023-06-01 17:19:41 +0530

    Java AssignmentOperators

---
+++ JAVA Assignments/Operator2.java
@@ -0,0 +1,23 @@
+public class Operator2{
+public static void main(String[] args){
+int num1=10,num2;
+
+num2=num1;
+System.out.println(num2);
+
+num2+=num1;
+System.out.println(num2);
+
+num2-=num1;
+System.out.println(num2);
+
+num2*=num1;
+System.out.println(num2);
+
+num2/=num1;
+System.out.println(num2);
+
+num2%=num1;
+System.out.println(num2);
+}
+}
