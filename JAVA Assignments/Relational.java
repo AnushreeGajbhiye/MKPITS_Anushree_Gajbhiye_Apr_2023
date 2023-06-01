9cfe304adb959e1d422a4488eb07532c72d5890d
AnushreeGajbhiiye <gajbhiyeanushree99@gmail.com>
2023-06-01 17:55:35 +0530

    Java Relational or Comparision operator

---
+++ JAVA Assignments/Relational.java
@@ -0,0 +1,38 @@
+class Relational{
+public static void main(String[] args){
+int num1=20,num2=30;
+if(num1!= num2){
+System.out.println("num1 is not equal to num2");
+}
+else{
+System.out.println("num1 is equal to num2");
+}
+
+
+if(num1<= num2){
+System.out.println("num1 is less then num2");
+}
+else{
+System.out.println("num1 is not less than num2");
+}
+if(num1>= num2){
+System.out.println("num1 is grester then num2");
+}
+else{
+System.out.println("num1 is not greater than num2");
+}
+if(num1 < num2){
+System.out.println("num1 is less then num2");
+}
+else{
+System.out.println("num1 is greater than num2");
+}
+if(num1 > num2){
+System.out.println("num1 is less then num2");
+}
+else{
+System.out.println("num1 is greater than num2");
+}
+
+}
+}
