f1ffe7c3017bc8bb9395bf32c4cea1455c1e82b3
AnushreeGajbhiiye <gajbhiyeanushree99@gmail.com>
2023-06-01 17:32:50 +0530

    Java UnaryOperators

---
+++ JAVA Assignments/UnaryOperator.java
@@ -0,0 +1,21 @@
+class UnaryOperator{
+public static void main(String[] args){
+int a=10;
+a++;
+System.out.println(a);
+
+ int b=20;
+ ++b;
+ System.out.println(b);
+
+ 
+ int c=10;
+c--;
+System.out.println(c);
+
+ int d=20;
+ --d;
+ System.out.println(d);
+ 
+}
+}
