43b68725a65bfd361b99fc7a1a36809d8a246e24
AnushreeGajbhiiye <gajbhiyeanushree99@gmail.com>
2023-06-05 00:09:44 +0530

    Concatination practice

---
+++ JAVA Assignments/Concatinate.java
@@ -0,0 +1,7 @@
+class Concatinate{
+public static void main(String[] args){
+int number1=10,number2=20;
+System.out.println("Addition of number1 and number2 is : " +number1+number2);//numbers are not contatinate
+System.out.println("Addition of number1 and number2 is : " +(number1+number2));//numbers are concatinate
+}
+}
