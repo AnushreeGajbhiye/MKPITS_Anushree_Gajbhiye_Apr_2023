ddfc2707fe228f850421d7149b3fb45f40538a9d
AnushreeGajbhiiye <gajbhiyeanushree99@gmail.com>
2023-06-15 23:54:58 +0530

    Java inheritance Example2

---
+++ JAVA Assignments/Bank.java
@@ -0,0 +1,21 @@
+class Bank{
+double height;
+double width;
+BankType bankType;
+}
+class Sbi extends Bank{
+String branch;
+String States;
+HeadOffice headOffice;
+}
+class Account extends Sbi{
+LoanType loanType;
+String accountType;
+String personalDetails;
+ }
+class Customer{
+long number;
+String customerId;
+String CustomerName;
+}
+ 
